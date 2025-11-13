c354c89e05b6cb4f0f355d303cb0d71e8e1c12a0
CarlosStx <csantacruze@yahoo.com.mx>
2025-11-13 08:55:52 -0600

    Update test_teu_structural.py

--- test_teu_structural.py
+++ test_teu_structural.py
@@ -2,6 +2,7 @@ import numpy as np
 from src.teu.structural_vacuum import c_eff
 from src.teu.dispersion import omega_k, schrodinger_epsilon
 
+
 def test_c_eff_positive():
     ce = c_eff(lambda_val=1.0, rho_v=1.0, kappa=1.0)
     assert ce > 0
